7ff56043ed119b6c3d221cb4fb12e06fd5a1e06f
rojomaldit <lucaserfurt@hotmail.com>
2022-03-02 08:50:53 -0300

    Simplify and remove unnecesary files

--- CardPayment/registration.php
+++ CardPayment/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register( 
 	\Magento\Framework\Component\ComponentRegistrar::MODULE, 
-	'ZenriseRentsModule_CardPayment',
+	'Zenrise_CardPayment',
 	__DIR__
 );
--- view/frontend/web/js/view/payment/CardModel.js
+++
@@ -1,8 +0,0 @@
-define(['uiComponent', 'Magento_Checkout/js/model/payment/renderer-list'], function (Component, rendererList) {
-	'use strict';
-	rendererList.push({
-		type: 'simple',
-		component: 'ZenriseRentsModule_CardPayment/js/view/payment/method-renderer/simple-method',
-	});
-	return Component.extend({});
-});
