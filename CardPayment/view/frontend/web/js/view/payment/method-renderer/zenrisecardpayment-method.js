b203fffbbe94633ce515bf36d45fdee5f88db984
rojomaldit <lucaserfurt@hotmail.com>
2022-03-07 08:36:58 -0300

    minor changes into checkout frontend

--- CardPayment/view/frontend/web/js/view/payment/method-renderer/zenrisecardpayment-method.js
+++ CardPayment/view/frontend/web/js/view/payment/method-renderer/zenrisecardpayment-method.js
@@ -1,19 +1,11 @@
-define(
-    [
-        'Magento_Checkout/js/view/payment/default'
-    ],
-    function (Component) {
-        'use strict';
-        return Component.extend({
-            defaults: {
-                template: 'Zenrise_CardPayment/payment/zenrisecardpayment'
-            },
-            getMailingAddress: function () {
-                return window.checkoutConfig.payment.checkmo.mailingAddress;
-            },
-            getInstructions: function () {
-                return window.checkoutConfig.payment.instructions[this.item.method];
-            },
-        });
-    }
-);
+define(['Magento_Checkout/js/view/payment/default'], function (Component) {
+	'use strict';
+	return Component.extend({
+		defaults: {
+			template: 'Zenrise_CardPayment/payment/zenrisecardpayment',
+		},
+		getClientId: function () {
+			return 'ClientId';
+		},
+	});
+});
